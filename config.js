2e2749059f93c057c0246315595241d16ff4d118
scanunicco <Scanuproductions@gmail.com>
2021-04-09 13:15:54 -0700

    Update todo url
    Update the to-do list url to enable its new transparency mode, allowing it to match the theme of the new-tab page.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const NUMBER_OF_IMAGES = 20; //Default Value: 20
 const TODO_ENABLED = true; //Default Value: true
 
 //The webpage displayed when the todo list is open. You could change this to something like evernote, a calender website, or even an online game if you want to be sneaky. For privacy and performance reasons, this webpage is not loaded until the to-do button is clicked
-const TODO_URL = "https://scanuproductions.com/webtools/To-Do/?embedded=true"; //Default Value: "https://scanuproductions.com/webtools/To-Do/?embedded=true"
+const TODO_URL = "https://scanuproductions.com/webtools/To-Do/?embedded=true&transparent=true"; //Default Value: "https://scanuproductions.com/webtools/To-Do/?embedded=true&transparent=true"
 
 //Whether the weather widget is enabled or not. Weather data is retrieved from wttr.in. If you are concerned about privacy you can disable the widged to prevent your new-tab page from connecting to wttr.in.
 const WEATHER_ENABLED = true; //Default Value: true
